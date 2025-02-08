4ee0eebc50477a048950c5c9e5a9f9d6105fd87e
ewk-elwa <edward.king@elwatech.com>
2025-02-08 09:07:02 +0000

    Updating build and deployment

--- build_images.sh
+++ build_images.sh
@@ -3,14 +3,14 @@
 # Function to build Docker image for recipebook
 build_recipebook() {
   echo "Building Docker image for recipebook..."
-  docker build -t recipebook:latest ./recipebook
+  docker build --platform linux/amd64 -t recipebook:latest ./recipebook
   echo "Docker image for recipebook built successfully."
 }
 
 # Function to build Docker image for todo
 build_todo() {
   echo "Building Docker image for todo..."
-  docker build -t todo:latest ./todo
+  docker build --platform linux/amd64 -t todo:latest ./todo
   echo "Docker image for todo built successfully."
 }
 
@@ -20,5 +20,14 @@ build_todo &
 
 # Wait for both builds to finish
 wait
-
 echo "Both Docker images built successfully."
+
+docker tag recipebook:latest elwatech/recipebook:latest
+docker tag todo:latest elwatech/todo:latest
+
+docker push elwatech/recipebook:latest &
+docker
+wait
+
+
+echo "Both Docker tagged and push to dockerhub built successfully."
